6037b1dd60821d7a5264880a16236f32f4275dd6
ivargas1030 <56612683+ivargas1030@users.noreply.github.com>
2019-12-07 10:36:34 -0600

    imdb changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,39 +1,87 @@
-var bookTitle = "Ender's game";
+//var bookTitle = "Ender's game";
 
 
 //------------------------------------------------
 //google API
 
-var queryGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q=" + bookTitle;
+//var queryGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q=" + bookTitle;
 
-$.ajax({
-    url: queryGoogleBooks,
-    method: "GET"
-}).then(function (respGoogleBooks) {
-    console.log(respGoogleBooks);
-})
+//$.ajax({
+    //url: queryGoogleBooks,
+   // method: "GET"
+//}).then(function (respGoogleBooks) {
+    //console.log(respGoogleBooks);
+//})
 
 //-----------------------------------
 //The Movie DB
 
-var movieDBKey = "a5b6a31636acf8a8c3c75e4575e245dd";
-var queryMovieDB = "https://api.themoviedb.org/3/search/movie?api_key=" + movieDBKey + "&query=" + bookTitle;
+//var movieDBKey = "a5b6a31636acf8a8c3c75e4575e245dd";
+//var queryMovieDB = "https://api.themoviedb.org/3/search/movie?api_key=" + movieDBKey + "&query=" + bookTitle;
 
-$.ajax({
-    url: queryMovieDB,
-    method: "GET"
-}).then(function (respMovieDB) {
-    console.log(respMovieDB);
-})
+//$.ajax({
+   // url: queryMovieDB,
+    //method: "GET"
+//}).then(function (respMovieDB) {
+    //console.log(respMovieDB);
+//})
 
 //-------------------------------------
 //
-var queryRawg = "https://api.rawg.io/api/games?search=Thor";
 
-$.ajax({
-    url: queryRawg,
-    method: "GET"
-}).then(function (respRawg) {
-    console.log(respRawg);
+
+
+//function displayGameInfo() {
+
+//var queryRawg = "https://api.rawg.io/api/games?search=Thor";
+
+//$.ajax({
+    //url: queryRawg,
+    //method: "GET"
+//}).then(function (respRawg) {
+    
+    //var gameMain = $("<div>");
+    //$(gameMain).addClass("game");
+//})
+
+//}
+
+
+
+    var movie = "Titanic";
+
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+    
+    //AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        var movieMain = $("<div>");
+        $(movieMain).addClass("movie");
+        
+        var rating = response.Rated;
+        var pRating = $("<p>");
+        $(pRating).text("Rating: " + rating);
+        $(movieMain).append(pRating);
+
+        var dateRelease = response.Released;
+          var pRelease = $("<p>");
+          $(pRelease).text("Released: " + dateRelease);
+          $(movieMain).append(pRelease);
+
+          
+          var plot = response.Plot;
+          var pPlot = $("<p>");
+          $(pPlot).text("Plot: " + plot);
+          $(movieMain).append(pPlot);
+          
+          var imgUrl = response.Poster;
+          var image = $("<img>").attr("src", imgUrl);
+          $(movieMain).append(image);
+          $("#movies-view").prepend(movieMain);
+
+          console.log(response);
+
+
 })
-
